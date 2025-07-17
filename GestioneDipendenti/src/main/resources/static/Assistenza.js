f38ffcdbdc8c0c09fecdc39c21a74fed0db6f15c
federicomolino <federicomolino450@gmail.com>
2025-07-17 15:23:04 +0200

    Add: Per invio richiesta aggiunta parte js, migliorato il front.

---
+++ GestioneDipendenti/src/main/resources/static/Assistenza.js
@@ -0,0 +1,34 @@
+function toggleTextAreaRichiesta(){
+    const textAreaRichiesta = document.getElementById("textAreaRichiesta");
+    const errorTextAreaDiv = document.getElementById("errorTextAreaDiv");
+    const errorLenghtTextAreaDiv = document.getElementById("errorLenghtTextAreaDiv");
+    const buttonInoltraRichiesta = document.getElementById("buttonInoltraRichiesta")
+
+    const value = textAreaRichiesta.value.trim();
+
+    if(value === ""){
+         errorTextAreaDiv.classList.remove("d-none");
+    }else{
+        errorTextAreaDiv.classList.add("d-none");
+    }
+
+    if(value.length === 255){
+        errorLenghtTextAreaDiv.classList.add("d-block");
+        errorLenghtTextAreaDiv.classList.remove("d-none");
+    }else{
+        errorLenghtTextAreaDiv.classList.add("d-none");
+        errorLenghtTextAreaDiv.classList.remove("d-block");
+    }
+
+    //Verifico quando abilitare il buttone
+    if(value !== "" && value.length <= 255){
+        buttonInoltraRichiesta.disabled = false;
+    }else{
+        buttonInoltraRichiesta.disabled = true;
+    }
+}
+
+/*Comportamento dinamico della select in automatico*/
+document.addEventListener("DOMContentLoaded", function () {
+    toggleTextAreaRichiesta();
+});
