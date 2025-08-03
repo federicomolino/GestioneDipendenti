b9a67316d887d18ca108dcfda230b83a0250a5a6
federicomolino <federicomolino450@gmail.com>
2025-08-03 18:49:48 +0200

    Fix: rindirizzamento dopo ricerca per mese

--- GestioneDipendenti/src/main/java/com/gestioneDipendeti/GestioneDipendenti/Repository/RuoloRepository.java
+++ GestioneDipendenti/src/main/java/com/gestioneDipendeti/GestioneDipendenti/Repository/RuoloRepository.java
@@ -1,7 +1,11 @@
 package com.gestioneDipendeti.GestioneDipendenti.Repository;
 
+import com.gestioneDipendeti.GestioneDipendenti.Entity.Dipendente;
 import com.gestioneDipendeti.GestioneDipendenti.Entity.Ruolo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RuoloRepository extends JpaRepository<Ruolo, Long> {
+    Optional<Ruolo> findByDipendente(Dipendente dipendente);
 }
